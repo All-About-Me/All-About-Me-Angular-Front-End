0a5ce0a579f0084e0468d18368b6f69d16304a8d
AngelaHsiao <angelaphsiao@gmail.com>
2022-08-18 07:20:02 -0700

    Add change function

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import User from 'src/app/models/User';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,8 +11,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  @ViewChild('editProfile',{static:true}) ngForm:NgForm;
 
   user: User = {} as User;  
+  formChangesSubscription: any;
 
   constructor(
     private authService: AuthService, 
@@ -19,10 +22,19 @@ export class UserProfileComponent implements OnInit {
     private router: Router) { }
 
   ngOnInit(): void {
-    this.user = this.authService.currentUser
+    this.user = this.authService.currentUser;
+    this.formChangesSubscription = this.ngForm.form.valueChanges.subscribe(x => {
+      if (document.getElementById('confirmUpdate')){ document.getElementById('confirmUpdate')?.remove()};
+      if (document.getElementById('refuseUpdate')){ document.getElementById('refuseUpdate')?.remove()}
+    })
+  }
+
+  ngOnDestroy() {
+    this.formChangesSubscription.unsubscribe();
   }
 
   onSubmit(editProfile:any){
+    if (!document.getElementById('confirmUpdate')){
     let updateForm = editProfile.value;
       console.log(updateForm);
     this.user.firstName=updateForm.inputFirstName;
@@ -35,10 +47,29 @@ export class UserProfileComponent implements OnInit {
       console.log(this.user);
     this._userService.updateUser(this.user).subscribe(
       (data)=>this.user=data);
+    this.confirmUpdate();
+    }
+    else {
+      this.refuseUpdate();
+    }
+  }
+
+  confirmUpdate():void{
     let tag :HTMLElement = document.createElement('p');
     let text = document.createTextNode("Successfully updated profile!");
     tag.appendChild(text);
+    tag.setAttribute('id', 'confirmUpdate');
     document.getElementById("edit")?.appendChild(tag);
+  }
+
+  refuseUpdate():void{
+    if (!document.getElementById('refuseUpdate')){
+    let tag:HTMLElement = document.createElement('p');
+    let text = document.createTextNode("You have already changed your profile");
+    tag.appendChild(text);
+    tag.setAttribute('id','refuseUpdate');
+    document.getElementById("edit")?.appendChild(tag);
+    }
   }
 
   resetPassword():void{
