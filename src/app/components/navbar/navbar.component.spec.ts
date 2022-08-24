44628c77efb9e1d0bd0af326eda7403df802d388
Michael Eldredge <michaelgeldredge@gmail.com>
2022-08-24 13:51:44 -0600

    Fixed Navbar test setup

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,7 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { MatMenuModule } from '@angular/material/menu';
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
@@ -10,21 +10,21 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, RouterTestingModule],      
+      imports: [HttpClientTestingModule, RouterTestingModule, MatMenuModule],      
       declarations: [ NavbarComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    // fixture = TestBed.createComponent(NavbarComponent);
-    // component = fixture.componentInstance;
-    // fixture.detectChanges();
+    fixture = TestBed.createComponent(NavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
   it('TestBed should be Truthy', () => {
     expect(TestBed).toBeTruthy();
   });
