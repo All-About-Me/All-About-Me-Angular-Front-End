408de4a1fcf07bc0c5b900b4d5a5f259dceb7a76
Michael Eldredge <michaelgeldredge@gmail.com>
2022-08-24 12:26:25 -0600

    Updated some (not all) component tests

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { NavbarComponent } from './navbar.component';
 
@@ -8,16 +10,17 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],      
       declarations: [ NavbarComponent ]
     })
     .compileComponents();
   });
 
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(NavbarComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+  beforeEach(() => {
+    // fixture = TestBed.createComponent(NavbarComponent);
+    // component = fixture.componentInstance;
+    // fixture.detectChanges();
+  });
 
   // it('should create', () => {
   //   expect(component).toBeTruthy();
