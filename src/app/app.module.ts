1d5a1c39de47b627c09a064c994256aa395c6b9e
Austin313 <32691123+Austin313@users.noreply.github.com>
2022-08-24 14:32:49 -0500

    commiting what i think fixes the ci issue
    commenting out raheens unused method,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 import { NgModule } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -26,6 +27,33 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 
+=======
+import { NgModule, ErrorHandler } from "@angular/core";
+import { FormBuilder, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AngularMaterialModule } from "./modules/angular-material.module";
+import { HttpClientModule } from "@angular/common/http";
+import { PostFeedPageComponent } from "./components/post-feed-page/post-feed-page.component";
+import { PostComponent } from "./components/post/post.component";
+import { CommentComponent } from "./components/comment/comment.component";
+import { UserCardComponent } from "./components/user-card/user-card.component";
+import { UserInitialsPipe } from "./pipes/user-initials.pipe";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { UserProfileComponent } from "./components/user-profile/user-profile.component";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { ResetPasswordComponent } from "./components/reset-password/reset-password.component";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { FlexLayoutModule } from "@angular/flex-layout";
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -39,7 +67,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     NavbarComponent,
     UserInitialsPipe,
     UserProfileComponent,
-    ResetPasswordComponent
+    ResetPasswordComponent,
   ],
   imports: [
     BrowserModule,
@@ -55,9 +83,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatMenuModule,
     MatButtonModule,
     MatIconModule,
-    MatDividerModule
+    MatDividerModule,
   ],
   providers: [FormBuilder],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
