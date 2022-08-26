42398ce6bb52955d4f7cb6f9ba1326f05526898b
Austin313 <32691123+Austin313@users.noreply.github.com>
2022-08-26 12:33:20 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   withCredentials: true,
-  baseUrl: "http://34.235.88.178:3600/",
+  baseUrl: "http://34.235.88.178:3600",
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://34.235.88.178:4200/',
