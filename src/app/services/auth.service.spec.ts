e9eb1fb5c665c2a8c17b15a2dd003f386388df87
Michael Eldredge <michaelgeldredge@gmail.com>
2022-08-26 10:53:06 -0600

    Auth test service now works

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AuthService } from './auth.service';
 
@@ -6,11 +9,17 @@ describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      //providers: [FormBuilder]
+    });
+   service = TestBed.inject(AuthService); 
   });
 
-  
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
   it('TestBed should be Truthy', () => {
     expect(TestBed).toBeTruthy();
   });
