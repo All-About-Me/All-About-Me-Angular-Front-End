3b30c026392cdbe7318efe820220c1e5f4104880
Michael Eldredge <michaelgeldredge@gmail.com>
2022-08-26 10:22:55 -0600

    More changes to user card tests

--- src/app/components/user-card/user-card.component.spec.ts
+++ src/app/components/user-card/user-card.component.spec.ts
@@ -1,12 +1,13 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { UserCardComponent } from './user-card.component';
 
 describe('UserCardComponent', () => {
-  let component: UserCardComponent;
-  let fixture: ComponentFixture<UserCardComponent>;
+  let component: HostUserCardComponent;
+  let fixture: ComponentFixture<HostUserCardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,9 +15,9 @@ describe('UserCardComponent', () => {
       declarations: [ UserCardComponent ]
     })
     .compileComponents();
-    fixture = TestBed.createComponent(UserCardComponent);
+    fixture = TestBed.createComponent(HostUserCardComponent);
     component = fixture.componentInstance;
-    // fixture.detectChanges();
+    fixture.detectChanges();
     
   });
   
@@ -27,4 +28,10 @@ describe('UserCardComponent', () => {
     expect(component).toBeTruthy();
   });
   
+  @Component({
+    selector: `host-component`,
+    template:`<app-user-card></app-user-card>`
+  })
+  class HostUserCardComponent{}
+
 });
