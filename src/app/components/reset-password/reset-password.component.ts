b1f390f2b685c93657812ec47c372c3a55d836f8
Jem D <jemimadumayas@gmail.com>
2022-08-29 12:39:21 -0700

    added alerts and style reset password

--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -11,7 +11,8 @@ import { UserService } from "src/app/services/user.service";
 })
 export class ResetPasswordComponent implements OnInit {
   user: User = {} as User;
-
+  info:boolean = false;
+  notice:String;
   constructor(
     private authService: AuthService,
     private _userService: UserService,
@@ -26,18 +27,28 @@ export class ResetPasswordComponent implements OnInit {
     let updateForm = pwdForm.value;
     console.log(updateForm);
     if (this.user.password === updateForm.currentPassword) {
+      if(updateForm.currentPassword === updateForm.newPassword) {
+        this.info=true;
+        this.notice = "Cannot be the same password!";
+      }
+      else {
       this.user.password = updateForm.newPassword;
-
-      console.log(this.user);
+      //console.log(this.user);
       this.authService
         .resetPassword(this.user)
         .subscribe((data) => (this.user = data));
-      let tag: HTMLElement = document.createElement("p");
-      let text = document.createTextNode("Successfully updated password!");
-      tag.appendChild(text);
-      document.getElementById("edit")?.appendChild(tag);
+        // alert("Password Updated");
+        this.info=true;
+        this.notice = "Password is updated!";
+      //let tag: HTMLElement = document.createElement("p");
+      //let text = document.createTextNode("Successfully updated password!");
+      //tag.appendChild(text);
+      //document.getElementById("edit")?.appendChild(tag);
+      }
     } else {
-      alert("Not correct password");
+      // alert("Current Password Incorect");
+      this.info=true;
+      this.notice = "Current password incorect!";
     }
   }
 }
