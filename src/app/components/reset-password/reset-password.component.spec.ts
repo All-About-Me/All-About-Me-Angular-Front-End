81d8739180205375e1884c6211340101127f0e6c
Austin313 <32691123+Austin313@users.noreply.github.com>
2022-08-30 17:37:31 -0500

    unit tests

--- src/app/components/reset-password/reset-password.component.spec.ts
+++ src/app/components/reset-password/reset-password.component.spec.ts
@@ -1,13 +1,31 @@
+import { formatNumber } from "@angular/common";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { FormsModule } from "@angular/forms";
+import { DebugElement } from "@angular/core";
+import { ComponentFixture, fakeAsync, TestBed, tick } from "@angular/core/testing";
+import { FormsModule, NgForm } from "@angular/forms";
 import { RouterTestingModule } from "@angular/router/testing";
+import User from "src/app/models/User";
 import { ResetPasswordComponent } from "./reset-password.component";
 
 describe("ResetPasswordComponent", () => {
   let component: ResetPasswordComponent;
   let fixture: ComponentFixture<ResetPasswordComponent>;
-
+  const testUser = new User(5,"testuser@gmail.com",'Test','User','','','','','','',8,'password')
+  const formdata = <NgForm>{
+    value: {
+      currentPassword: 'password',
+    newPassword: 'pass'
+    }
+    
+  }
+  const pwddata = <NgForm>{
+    value: {
+      currentPassword: 'password',
+    newPassword: 'password'
+    }
+    
+  }
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, RouterTestingModule, FormsModule],
@@ -24,4 +42,25 @@ describe("ResetPasswordComponent", () => {
   it("should be created", () => {
     expect(component).toBeTruthy();
   });
+
+  it('onCall should change password', fakeAsync(()=>{
+    component.user = testUser;
+    fixture.detectChanges();
+    console.log("formdata:")
+    component.onCall(formdata);
+    fixture.detectChanges();
+    tick();
+    expect(component.user.password).toEqual('pass')
+    tick();
+  }))
+
+  it('onCall should not allow same password', fakeAsync(()=>{
+    component.user = testUser;
+    fixture.detectChanges();
+    component.onCall(pwddata);
+    fixture.detectChanges();
+    tick();
+    expect(component.user.password).toEqual('password')
+  }))
 });
+
