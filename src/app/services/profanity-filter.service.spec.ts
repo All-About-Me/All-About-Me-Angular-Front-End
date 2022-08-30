9509746290835c3b183f9ba0c5cdea4d5257f5f3
Mike Kight <102483977+Mdkight@users.noreply.github.com>
2022-08-30 14:44:56 -0500

    fixed broken video link functionality

--- src/app/services/profanity-filter.service.spec.ts
+++ src/app/services/profanity-filter.service.spec.ts
@@ -13,4 +13,16 @@ describe('ProfanityFilterService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should refuse a banned word', () => {
+    let bannedWord:string =service.bannedWords[1]
+    let accepted = service.validatePost(bannedWord)
+    expect(accepted).toBeFalsy()
+  })
+
+  it('should accept a non banned word', () => {
+    let nonBannedWord:string ='the quick brown fox jumped over the lazy dog'
+    let accepted = service.validatePost(nonBannedWord)
+    expect(accepted).toBeTruthy()
+  })
 });
